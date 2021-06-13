f786452711412b9e3240cc1036ec3de8dad82a52
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-06-13 10:26:20 +0530

    Update sg.py

--- ShasaBot/modules/sg.py
+++ ShasaBot/modules/sg.py
@@ -1,7 +1,7 @@
+# Copyright (C) 2021 AsunaRobot
 # made by @The_Ghost_Hunter on Telegram. 
 # github account : https://github.com/HuntingBots/
 # This file is part of AsunaRobot (Telegram Bot)
-# Copyright ©AsunaRobot 2021
 
 import datetime 
 import asyncio
@@ -79,7 +79,7 @@ async def _(event):
 
         return
 
-    lol = await event.reply("```Geting Info```")
+    lol = await event.reply("```Processing```")
 
     async with ubot.conversation(chat) as conv:
 
